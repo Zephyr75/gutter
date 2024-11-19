86c99f95434b49f4b139963f64b6dc4279851cd5
Antoine <antoine.familleroger@gmail.com>
2024-11-19 23:49:22 +0100

    Added main method to test

--- core/app.go
+++ core/app.go
@@ -20,18 +20,16 @@ import (
 )
 
 type App struct {
-  Name string
-  Width int
-  Height int
-  Window *glfw.Window
+	Name   string
+	Width  int
+	Height int
+	Window *glfw.Window
 }
 
-
-
 func init() {
-    // GLFW: This is needed to arrange that main() runs on main thread.
-    // See documentation for functions that are only allowed to be called from the main thread.
-    runtime.LockOSThread()
+	// GLFW: This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
 }
 
 func (app App) Quit() {
@@ -39,135 +37,127 @@ func (app App) Quit() {
 }
 
 func (app App) Run(widget func(app App) ui.UIElement) {
-    err := glfw.Init()
-    if err != nil {
-        panic(err)
-    }
-    defer glfw.Terminate()
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
 
-    utils.RESOLUTION_X = app.Width
-    utils.RESOLUTION_Y = app.Height
+	utils.RESOLUTION_X = app.Width
+	utils.RESOLUTION_Y = app.Height
 
-    window, err := glfw.CreateWindow(utils.RESOLUTION_X, utils.RESOLUTION_Y, "Gutter", nil, nil)
-    if err != nil {
-        panic(err)
-    }
+	window, err := glfw.CreateWindow(utils.RESOLUTION_X, utils.RESOLUTION_Y, "Gutter", nil, nil)
+	if err != nil {
+		panic(err)
+	}
 
-    app.Window = window
+	app.Window = window
 
-    window.MakeContextCurrent()
+	window.MakeContextCurrent()
 
-    err = gl.Init()
-    if err != nil {
-        panic(err)
-    }
+	err = gl.Init()
+	if err != nil {
+		panic(err)
+	}
 
-    var texture uint32
-    {
-        gl.GenTextures(1, &texture)
+	var texture uint32
+	{
+		gl.GenTextures(1, &texture)
 
-        gl.BindTexture(gl.TEXTURE_2D, texture)
-        gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-        gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-        gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-        gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+		gl.BindTexture(gl.TEXTURE_2D, texture)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
 
-        gl.BindImageTexture(0, texture, 0, false, 0, gl.WRITE_ONLY, gl.RGBA8)
-    }
+		gl.BindImageTexture(0, texture, 0, false, 0, gl.WRITE_ONLY, gl.RGBA8)
+	}
 
-    var framebuffer uint32
-    {
-        gl.GenFramebuffers(1, &framebuffer)
-        gl.BindFramebuffer(gl.FRAMEBUFFER, framebuffer)
-        gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0)
+	var framebuffer uint32
+	{
+		gl.GenFramebuffers(1, &framebuffer)
+		gl.BindFramebuffer(gl.FRAMEBUFFER, framebuffer)
+		gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0)
 
-        gl.BindFramebuffer(gl.READ_FRAMEBUFFER, framebuffer)
-        gl.BindFramebuffer(gl.DRAW_FRAMEBUFFER, 0)
-    }
+		gl.BindFramebuffer(gl.READ_FRAMEBUFFER, framebuffer)
+		gl.BindFramebuffer(gl.DRAW_FRAMEBUFFER, 0)
+	}
 
+	///
+	i := 0
+	time := glfw.GetTime()
+	///
 
-    ///
-    i := 0
-    time := glfw.GetTime()
-    ///
+	lastInstance := ""
+	var flippedImg *image.NRGBA
 
-    lastInstance := ""
-    var flippedImg *image.NRGBA
+	lastMap := map[string]bool{}
+	areas := []ui.Area{}
 
-    lastMap := map[string]bool{}
-    areas := []ui.Area{}
+	for !window.ShouldClose() {
 
-    for !window.ShouldClose() {
+		var w, h = window.GetSize()
 
-        var w, h = window.GetSize()
+		utils.RESOLUTION_X = w
+		utils.RESOLUTION_Y = h
 
-        utils.RESOLUTION_X = w
-        utils.RESOLUTION_Y = h
+		var img = image.NewRGBA(image.Rect(0, 0, w, h))
 
-        var img = image.NewRGBA(image.Rect(0, 0, w, h))
+		// -------------------------
+		// MODIFY OR LOAD IMAGE HERE
+		// -------------------------
 
-        // -------------------------
-        // MODIFY OR LOAD IMAGE HERE
-        // -------------------------
+		instance := widget(app)
 
-        instance := widget(app)
-        
+		equal := true
+		for _, area := range areas {
+			if ui.MouseInBounds(window, area) != lastMap[area.ToString()] {
+				equal = false
+			}
+			if ui.MouseInBounds(window, area) && window.GetMouseButton(glfw.MouseButtonLeft) == glfw.Press {
+				area.Function()
+			}
+		}
 
+		if lastInstance != instance.ToString() || !equal {
+			lastInstance = instance.ToString()
+			areas = instance.Draw(img, window)
+			// Remove all empty areas
+			newAreas := []ui.Area{}
+			for _, area := range areas {
+				if area.Left != 0 || area.Right != 0 || area.Top != 0 || area.Bottom != 0 {
+					newAreas = append(newAreas, area)
+				}
+			}
+			areas = newAreas
+			flippedImg = imaging.FlipV(img)
+		}
+		for _, area := range areas {
+			lastMap[area.ToString()] = ui.MouseInBounds(window, area)
+		}
 
-        equal := true
-        for _, area := range areas {
-          if ui.MouseInBounds(window, area) != lastMap[area.ToString()] {
-            equal = false
-          }
-          if ui.MouseInBounds(window, area) && window.GetMouseButton(glfw.MouseButtonLeft) == glfw.Press {
-            area.Function()
-          }
-        }
-    
-        if lastInstance != instance.ToString() || !equal {
-          lastInstance = instance.ToString()
-          areas = instance.Draw(img, window)
-          // Remove all empty areas
-          newAreas := []ui.Area{}
-          for _, area := range areas {
-            if area.Left != 0 || area.Right != 0 || area.Top != 0 || area.Bottom != 0 {
-              newAreas = append(newAreas, area)
-            }
-          }
-          areas = newAreas
-          flippedImg = imaging.FlipV(img)
-        }
-        for _, area := range areas {
-          lastMap[area.ToString()] = ui.MouseInBounds(window, area)
-        }
-          
-        // window.SetShouldClose(true)
-        // -------------------------
+		// window.SetShouldClose(true)
+		// -------------------------
 
-        gl.BindTexture(gl.TEXTURE_2D, texture)
-        gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA8, int32(w), int32(h), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(flippedImg.Pix))
+		gl.BindTexture(gl.TEXTURE_2D, texture)
+		gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA8, int32(w), int32(h), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(flippedImg.Pix))
 
-        gl.BlitFramebuffer(0, 0, int32(w), int32(h), 0, 0, int32(w), int32(h), gl.COLOR_BUFFER_BIT, gl.LINEAR)
+		gl.BlitFramebuffer(0, 0, int32(w), int32(h), 0, 0, int32(w), int32(h), gl.COLOR_BUFFER_BIT, gl.LINEAR)
 
-        window.SwapBuffers()
-        glfw.PollEvents()
+		window.SwapBuffers()
+		glfw.PollEvents()
 
-        ///
-        i++
-        if glfw.GetTime()-time > 1 {
-          fmt.Printf("\rFPS: %d", i)
-          i = 0
-          time = glfw.GetTime()
-        }
-        ///
+		///
+		i++
+		if glfw.GetTime()-time > 1 {
+			fmt.Printf("\rFPS: %d", i)
+			i = 0
+			time = glfw.GetTime()
+		}
+		///
 
+		// TODO: Store loaded image instead of its name
+		// TODO: Store last resized image version to avoid recomputing if change is not related to image size
 
-        // TODO: Store loaded image instead of its name
-        // TODO: Store last resized image version to avoid recomputing if change is not related to image size
-
-
-
-    }
+	}
 }
-
-
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"image/color"
+
+	"github.com/Zephyr75/gutter/core"
+	"github.com/Zephyr75/gutter/ui"
+	// "fmt"
+)
+
+var (
+	counter int = 10
+)
+
+func main() {
+	myApp := core.App{
+		Name:   "Gutter",
+		Width:  800,
+		Height: 600,
+	}
+	myApp.Run(MainWindow)
+}
+
+func MainWindow(app core.App) ui.UIElement {
+	return ui.Row{
+		Style: ui.Style{
+			Color: color.Transparent,
+		},
+		Children: []ui.UIElement{
+			ui.Button{
+				Properties: ui.Properties{
+					Alignment: ui.AlignmentTop,
+					Size: ui.Size{
+						Scale:  ui.ScalePixel,
+						Width:  100,
+						Height: 100,
+					},
+				},
+				Function: func() {
+					app.Quit()
+				},
+				Style: ui.Style{
+					Color: green,
+				},
+			},
+			ui.Column{
+				Properties: ui.Properties{
+					Padding: ui.PaddingSideBySide(ui.ScaleRelative, 0, 25, 25, 0),
+				},
+				Style: ui.Style{
+					Color: color.White,
+				},
+				Children: []ui.UIElement{
+					ui.Button{
+						Properties: ui.Properties{
+							Size: ui.Size{
+								Scale:  ui.ScaleRelative,
+								Width:  50,
+								Height: 50,
+							},
+						},
+						Function: func() {
+							counter += 1
+						},
+						Style: ui.Style{
+							Color: green,
+						},
+						// Image:      "white_on_black.png",
+						// HoverImage: "black_on_white.png",
+					},
+					ui.Button{
+						Properties: ui.Properties{
+							Size: ui.Size{
+								Scale:  ui.ScaleRelative,
+								Width:  50,
+								Height: 100,
+							},
+						},
+						Function: func() {
+							counter -= 1
+						},
+						Style: ui.Style{
+							Color: red,
+							// BorderColor: white,
+							// BorderWidth: 10,
+							// CornerRadius: 25,
+						},
+						Child: ui.Text{
+							Properties: ui.Properties{
+								Alignment: ui.AlignmentTopLeft,
+								//Padding:   ui.PaddingEqual(ui.ScalePixel, 100),
+								Size: ui.Size{
+									Scale:  ui.ScalePixel,
+									Width:  100,
+									Height: 50,
+								},
+							},
+							StyleText: ui.StyleText{
+								Font:      "Comfortaa.ttf",
+								FontSize:  counter,
+								FontColor: black,
+							},
+						},
+						// Image:      "white_on_black.png",
+						// HoverImage: "black_on_white.png",
+					},
+					ui.Container{
+						Properties: ui.Properties{
+							Size: ui.Size{
+								Scale:  ui.ScaleRelative,
+								Width:  50,
+								Height: 50,
+							},
+						},
+						Style: ui.Style{
+							// BorderWidth: 10,
+							// BorderColor: white,
+							// CornerRadius: 25,
+							Color: color.Transparent,
+							// ShadowWidth: 10,
+							// ShadowAlignment: ui.AlignmentBottom,
+						},
+						// Image: "white_on_black.png",
+					},
+				},
+			},
+			ui.Container{
+				Style: ui.Style{
+					Color: red,
+				},
+				Child: ui.Text{
+					Properties: ui.Properties{
+						Alignment: ui.AlignmentTopLeft,
+						//Padding:   ui.PaddingEqual(ui.ScalePixel, 100),
+						Size: ui.Size{
+							Scale:  ui.ScalePixel,
+							Width:  100,
+							Height: 50,
+						},
+					},
+					StyleText: ui.StyleText{
+						Font:      "Comfortaa.ttf",
+						FontSize:  counter,
+						FontColor: black,
+					},
+				},
+			},
+		},
+	}
+}
+
+var (
+	green       = color.RGBA{158, 206, 106, 255}
+	white       = color.RGBA{192, 202, 245, 255}
+	blue        = color.RGBA{122, 162, 247, 255}
+	red         = color.RGBA{247, 118, 142, 255}
+	black       = color.RGBA{26, 27, 38, 255}
+	transparent = color.RGBA{0, 0, 0, 0}
+)
